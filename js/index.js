807384d0621b548aa41620489902b98cadb85dcd
patrick kisyula <pmusyimi42@gmail.com>
2022-11-19 11:33:00 +0300

    added mouse events

--- js/index.js
+++ js/index.js
@@ -1,4 +1,10 @@
 document.addEventListener("DOMContentLoaded", function(){
     alert("The document has loaded succesfully")
-
+    const title1 = document.querySelector('h1')
+    title1.addEventListener('mouseover',function(){
+        title1.style.color = 'blue';
+    })
+    title1.addEventListener('mouseout', function(){
+        title1.style.color = 'black';
+    })
 })
